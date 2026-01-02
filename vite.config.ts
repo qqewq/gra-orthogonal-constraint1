0382561188bb1e06ea8985e16ddeedf644e8be92
oleg bits <bits.oleg@bk.ru>
2026-01-02 09:48:13 +0300

    Update vite.config.ts
    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/gra-orthogonal-constraint1/",
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
